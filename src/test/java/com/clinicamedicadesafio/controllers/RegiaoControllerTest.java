f6b2fb22fbee9a05c5d61a33ab824be473797bd8
Matheus Vitor Ferreira <153767841+MatheusVitorFerreira@users.noreply.github.com>
2024-07-03 12:55:46 -0300

    atualização de rotina para verificar a integridade do aplicativo
    Realizou manutenção e verificações de rotina para garantir que todos os sistemas estejam operacionais:
    - Acesso verificado ao console do banco de dados H2
    - Configurações de segurança confirmadas
    - Verificação de erros nos logs do aplicativo

--- src/test/java/com/clinicamedicadesafio/controllers/RegiaoControllerTest.java
+++ src/test/java/com/clinicamedicadesafio/controllers/RegiaoControllerTest.java
@@ -1,9 +1,6 @@
 package com.clinicamedicadesafio.controllers;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
